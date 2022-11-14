937cc6712ffed97c8e90761be89c32b180762b15
Michael Ritsch <office@michael-ritsch.com>
2022-11-14 14:59:34 +0100

    added git workflow

---
+++ lawi-subscription-handling/lawi-subscription-handling.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ *
+ *
+ * @package           lawi-subscription-handling
+ * @author            wp-stars
+ * @copyright         2019 wp-stars gmbh
+ *
+ * @wordpress-plugin
+ * Plugin Name:
+ * Plugin URI:        https://wp-stars.com
+ * Description:       This plugin handles all the subscriptions for landwirt
+ * Version:           %%version%%
+ * Requires PHP:      8.1
+ * Author:            wp-stars gmbh
+ * Author URI:        https://wp-stars.com
+ * Text Domain:
+ */
