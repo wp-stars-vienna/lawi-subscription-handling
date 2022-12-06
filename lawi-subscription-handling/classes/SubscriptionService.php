c51a212c8d98a3aac1b695148c22b2a5bbf98eb0
Will Nahmens <willnahmens@Pauls.local>
2022-12-06 13:43:08 +0100

    fix: working version of updating sub renewal dates

--- lawi-subscription-handling/classes/SubscriptionService.php
+++ lawi-subscription-handling/classes/SubscriptionService.php
@@ -5,6 +5,7 @@ namespace wps\lawi;
 use \DateTime;
 use \WC_Order;
 
+
 class SubscriptionService
 {
 
@@ -17,16 +18,22 @@ class SubscriptionService
     {
         // TODO: add functionality to prevent multiple subs of the same type
         // TODO: prevent subs & regular items being mixed in cart
-        add_action('woocommerce_payment_complete', array($this, 'wps_upate_next_payment_datetime'));
-
+        add_action('woocommerce_payment_complete', array($this, 'wps_upate_next_payment'));
     }
 
-    public function wps_upate_next_payment_datetime($order_id) {
+    public function wps_upate_next_payment($order_id) {
         if(wcs_order_contains_subscription($order_id)) {
             $order = new WC_Order( $order_id );
-        
-            // epaper-startdate
-            $start_date = $order->get_meta('epaper-startdate');
+
+            $start_date = $order->get_meta('_epaper_startdate');
+
+            $next_payment = new \Datetime();
+            $next_payment->setTimestamp(strtotime($start_date . " +1 month"));
+            $next_payment->setTime(0, 0);
+            $next_payment = $next_payment->format('Y-m-d H:i:s');
+
+            $start_date = new \DateTime($start_date);
+            $start_date = $start_date->format('Y-m-d H:i:s');
 
             $subscriptionsOfOrder = wcs_get_subscriptions_for_order( $order_id );
             $subscription = $subscriptionsOfOrder[array_key_first($subscriptionsOfOrder)];
@@ -34,14 +41,15 @@ class SubscriptionService
             if($subscription) {
                 $dates = array();
                 $dates['start'] = $start_date;
-                $dates['next_payment'] = date("Y-m-d h:i:s", strtotime($start_date . " +1 month"));
-        
+                $dates['next_payment'] = $next_payment;
+
                 try {
                     $subscription->update_dates( $dates, 'gmt' );
                     wp_cache_delete( $order_id, 'posts' );
                 } catch ( Exception $e ) {
                     wcs_add_admin_notice( $e->getMessage(), 'error' );
                 }
+
             }
         }
     }
